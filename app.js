7e565f39271af4da60f484f29b7934b60cb3e50a
LasseOergaardAAU <lorgaa23@student.aau.dk>
2024-03-05 17:00:13 +0100

    wip auth

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const expessLayouts = require("express-ejs-layouts");
-
+const cookieParser = require("cookie-parser");
+const { cookieJwtAuth } = require("./middleware/cookieJwtAuth");
 // Init app and middleware
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
-app.set("layout", "./layouts/layout")
+app.set("layout", "./layouts/layout");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.use(express.json());
 app.use(expessLayouts);
-
+app.use(cookieParser());
 
 //Routes
 const indexRouter = require("./routes/index");
@@ -18,9 +19,6 @@ app.use("/", indexRouter);
 
 const userRouter = require("./routes/user");
 app.use("/user", userRouter);
-
-
-
 
 
 //Port
