babf6a045f41de97c80f8881e6dd6a1d87358cb9
MagnusHK3 <magnus.h.kristensen@gmail.com>
2024-03-06 12:33:10 +0100

    fixes

--- routes/user.js
+++ routes/user.js
@@ -15,17 +15,6 @@ connectToDb((err) => {
 
 //routes
 
-router.get("/", (req, res) => {
-  const decodedUser = jwt.verify(req.cookies.token, process.env.JWTSECRET);
-
-  db.collection("users")
-    .findOne({ username: decodedUser.username })
-    .then((user) => {
-      console.log(user);
-      res.render("user", { user });
-    });
-});
-
 router.get("/events", (req, res) => {
   const decodedUser = jwt.verify(req.cookies.token, process.env.JWTSECRET);
   let events = [];
@@ -69,60 +58,6 @@ router.get("/groups", (req, res) => {
           res.render("groups", { groups });
         });
     });
-});
-
-router.post("/register", async (req, res) => {
-  try {
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    const user = {
-      username: req.body.username,
-      password: hashedPassword,
-    };
-
-    db.collection("users")
-      .insertOne(user)
-      .then(() => {
-        res.render("user");
-      });
-  } catch {
-    res.status(500).send();
-  }
-});
-
-router.post("/login", async (req, res) => {
-  //Tries to find user
-  let logedInUser = await db
-    .collection("users")
-    .findOne({ username: req.body.username });
-
-  if (!logedInUser) {
-    return res.status(400).send("cannot find user");
-  }
-
-  try {
-    const passMatch = await bcrypt.compare(
-      req.body.password,
-      logedInUser.password
-    );
-
-    if (passMatch) {
-      //Creates jwt token
-      const token = jwt.sign(logedInUser, process.env.JWTSECRET, {
-        expiresIn: "30m",
-      });
-
-      //sets cookie in browser
-      res.cookie("token", token, {
-        httpOnly: true,
-      });
-
-      res.render("user");
-    } else {
-      res.send("not allowed");
-    }
-  } catch {
-    res.status(500).send();
-  }
 });
 
 module.exports = router;
