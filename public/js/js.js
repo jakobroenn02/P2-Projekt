b6cc4e3f772d37b76644c5d8562a6436cf942faa
Jonas Stæhr <GQ08WF@student.aau.dk>
2024-03-06 16:45:14 +0100

    wip

--- public/js/js.js
+++ public/js/js.js
@@ -1,3 +1,17 @@
 function dropdown_funktion(){
+    document.getElementById("mydropdown").classList.toggle("show")
+}
 
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')){
+        var dropdown = document.getElementsByClassName("dropdown-content");
+        var i
+        for (i= 0; i < dropdown.length; i++) {
+            var opendropdown = dropdown[i];
+            if (opendropdown.classList.contains('show')){
+                opendropdown.classList.remove('show');
+            } 
+            
+        }
+    }
 }
