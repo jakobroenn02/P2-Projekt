b22b3741cf9b3eca4c727c13b4be2a0286c0803f
LasseOergaardAAU <lorgaa23@student.aau.dk>
2024-03-07 20:06:34 +0100

    search interests functionality

--- public/js/js.js
+++ public/js/js.js
@@ -1,15 +1,26 @@
-
 const settingsButton = document.querySelector(".header-settings-icon");
 const dropdown = document.querySelector(".settings-dropdown");
 
 settingsButton.addEventListener("click", (e) => {
-    if(window.getComputedStyle(dropdown).display == "none") {
-        dropdown.style.display = "flex";
+  if (window.getComputedStyle(dropdown).display == "none") {
+    dropdown.style.display = "flex";
+  } else {
+    dropdown.style.display = "none";
+  }
+});
+
+const inputInterests = document.querySelector(".interestsSearchInput");
+const interestsContainer = document.querySelector(".interestsContainer");
+
+inputInterests.addEventListener("keyup", (e) => {
+  var children = interestsContainer.children;
+
+  for (var i = 0; i < children.length; i++) {
+    if (!children[i].innerText.toLowerCase().includes(inputInterests.value.toLowerCase())) {
+        children[i].style.display = "none"
     } else {
-        dropdown.style.display = "none";
+        children[i].style.display = "block"
+        
     }
-})
-
-
-
-
+  }
+});
