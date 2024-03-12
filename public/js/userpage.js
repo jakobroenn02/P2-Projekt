01e382e41e5690b804ea5c52d938a92343791215
Jakob <jakob.h.roenn@gmail.com>
2024-03-12 09:38:07 +0100

    added button for chaning picture

--- public/js/userpage.js
+++ public/js/userpage.js
@@ -1,30 +1,33 @@
-
 const settingsButton = document.querySelector(".header-settings-icon");
 const dropdown = document.querySelector(".settings-dropdown");
 const pictureProfile = document.querySelector(".profile-picture-picture");
 const pictureModal = document.querySelector(".modal");
 const closeModal = document.querySelector(".close-mark");
-
+const modalButton = document.querySelector(".change-picture-button");
 
 settingsButton.addEventListener("click", (e) => {
-    if(window.getComputedStyle(dropdown).display == "none") {
-        dropdown.style.display = "flex";
-    } else {
-        dropdown.style.display = "none";
-    }
-})
+  if (window.getComputedStyle(dropdown).display == "none") {
+    dropdown.style.display = "flex";
+  } else {
+    dropdown.style.display = "none";
+  }
+});
 
 
-pictureProfile.addEventListener("click", () => {
+   
+   modalButton.addEventListener("click", () => {
     pictureModal.style.display = "flex";
-});
-closeModal.addEventListener("click", () => {
+   })
+
+  pictureProfile.addEventListener("click", () => {
+    pictureModal.style.display = "flex";
+  })
+
+  closeModal.addEventListener("click", () => {
     pictureModal.style.display = "none";
-});
-window.addEventListener("click", (event) => {
+  });
+  window.addEventListener("click", (event) => {
     if (event.target == pictureModal) {
-        pictureModal.style.display = "none";
+      pictureModal.style.display = "none";
     }
-});
-
-
+  });
