cf5279f63cb472314c9b3534ae4fbc0fbd62a034
MagnusHK3 <magnus.h.kristensen@gmail.com>
2024-03-12 12:34:52 +0100

    Cleaner design for userpage and buttons to make input fields editable

--- public/js/userpage.js
+++ public/js/userpage.js
@@ -5,9 +5,21 @@ const pictureProfile = document.querySelector(".profile-picture-picture");
 const pictureModal = document.querySelector(".modal");
 const closeModal = document.querySelector(".close-mark");
 
+const editFirstName = document.getElementById("newFirstName");
+const toggleEditFirstName = document.querySelector(".firstname-toggle-icon i");
+const editLastName = document.getElementById("newLastName");
+const toggleEditLastName = document.querySelector(".lastname-toggle-icon i");
+const editUsername = document.getElementById("newUsername");
+const toggleEditUsername = document.querySelector(".username-toggle-icon i");
+const editEmail = document.getElementById("newEmail");
+const toggleEditEmail = document.querySelector(".email-toggle-icon i");
+const editPassword = document.getElementById("newPassword");
+const toggleEditPassword = document.querySelector(".password-toggle-icon i");
+const editConfirmPassword = document.getElementById("confirmNewPassword");
+const toggleEditConfirmPassword = document.querySelector(".confirm-password-toggle-icon i");
 
 settingsButton.addEventListener("click", (e) => {
-    if(window.getComputedStyle(dropdown).display == "none") {
+    if (window.getComputedStyle(dropdown).display == "none") {
         dropdown.style.display = "flex";
     } else {
         dropdown.style.display = "none";
@@ -24,6 +36,95 @@ closeModal.addEventListener("click", () => {
 window.addEventListener("click", (event) => {
     if (event.target == pictureModal) {
         pictureModal.style.display = "none";
+    }
+});
+
+toggleEditPassword.addEventListener("mouseover", function () {
+    editPassword.type = "text";  
+});
+toggleEditPassword.addEventListener("mouseout", function () {
+    editPassword.type = "password";
+});
+
+toggleEditConfirmPassword.addEventListener("mouseover", function () {
+    editConfirmPassword.type = "text";
+});
+toggleEditConfirmPassword.addEventListener("mouseout", function () {
+    editConfirmPassword.type = "password";
+});
+
+toggleEditFirstName.addEventListener("click", function () {
+    if (editFirstName.readOnly === true) {
+        editFirstName.readOnly = false;
+    } else {
+        editFirstName.readOnly = true;
+    }
+
+});
+
+toggleEditLastName.addEventListener("click", function () {
+    if (editLastName.readOnly === true) {
+        editLastName.readOnly = false;
+    } else {
+        editLastName.readOnly = true;
+    }
+});
+
+toggleEditUsername.addEventListener("click", function () {
+    if (editUsername.readOnly === true) {
+        editUsername.readOnly = false;
+    } else {
+        editUsername.readOnly = true;
+    }
+});
+
+toggleEditEmail.addEventListener("click", function () {
+    if (editEmail.readOnly === true) {
+        editEmail.readOnly = false;
+    }else {
+        editEmail.readOnly = true;
+    }
+});
+
+toggleEditPassword.addEventListener("click", function () {
+    if (editPassword.readOnly === true) {
+        editPassword.readOnly = false;
+        editPassword.classList.remove("fas fa-edit icon");
+    } else {
+        editPassword.readOnly = true;
+        editPassword.classList.add("fas fa-edit icon");
+    }
+});
+
+toggleEditConfirmPassword.addEventListener("click", function () {
+    if (editConfirmPassword.readOnly === true) {
+        editConfirmPassword.readOnly = false;
+        editPassword.classList.remove("fas fa-edit icon");
+    } else {
+        editConfirmPassword.readOnly = true;
+        editPassword.classList.add("fas fa-edit icon");
+    }
+});
+/* Ved ikke hvordan jeg skal få dette til at fungerere????? Vil gerne have den til at modtage de nye informationer og overskrive de gamle informationer :D */
+router.post("/", async (req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.newPassword, 10);
+        const user = {
+            name: { firstName: req.body.newFirstName, lastName: req.body.newLastName },
+            password: hashedPassword,
+            bio: "",
+            age: 0,
+            location: "",
+            groupIds: [],
+            interests: [],
+            eventIds: [],
+            username: req.body.newUsername,
+            email: req.body.newEmail,
+        };
+        db.collection("users")
+            .insertOne(user)
+    } catch {
+        res.status(500).send();
     }
 });
 
