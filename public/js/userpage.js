4d47e8b4f24b7025147ebf41394162cf644c0b4c
Jakob <jakob.h.roenn@gmail.com>
2024-03-13 13:15:54 +0100

    Error when opening modal

--- public/js/userpage.js
+++ public/js/userpage.js
@@ -1,9 +1,12 @@
+const { response } = require("express");
+
 const settingsButton = document.querySelector(".header-settings-icon");
 const dropdown = document.querySelector(".settings-dropdown");
 const pictureProfile = document.querySelector(".profile-picture-picture");
 const pictureModal = document.querySelector(".pictures-modal");
 const closeModal = document.querySelector(".close-mark");
 const modalButton = document.querySelector(".change-picture-button");
+const selectNewPicture = document.querySelectorAll("modal-pictures-options-container"); 
 
 settingsButton.addEventListener("click", (e) => {
   if (window.getComputedStyle(dropdown).display == "none") {
@@ -25,3 +28,24 @@ closeModal.addEventListener("click", () => {
   pictureModal.style.display = "none";
 });
 
+selectNewPicture.forEach((picture) => {
+  picture.addEventListener("click", (e) => {
+    pictureProfile.src = e.target.src;
+    pictureModal.style.display = "none";
+    
+    fetch('/updateProfilePicture', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ userId: user._id,
+        profileImageId: e.target.id }),
+  })
+  .then((response) => response.json())
+  .then(data => {
+    console.log(data);
+    user.profileImageId = data.profileImageId;
+  })
+  .catch((error) => console.log('error:', error));
+});
+});
