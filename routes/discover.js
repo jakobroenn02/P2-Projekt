40a4881ac44c53422b159327b9cb2dd5f0a97402
LasseOergaardAAU <lorgaa23@student.aau.dk>
2024-03-14 13:31:53 +0100

    join group functionality

--- routes/discover.js
+++ routes/discover.js
@@ -26,9 +26,41 @@ router.get("/:id", async (req, res) => {
       .collection("groups")
       .findOne({ _id: new ObjectId(req.params.id) });
 
-    const groupMemberAmount = group.userIds.length
-    group.groupMemberAmount = groupMemberAmount
-    res.render("discoverGroup", { isLoggedIn: true, group});
+    const groupMemberAmount = group.userIds.length;
+    group.groupMemberAmount = groupMemberAmount;
+    res.render("discoverGroup", { isLoggedIn: true, group });
+  }
+});
+
+router.post("/:id/join", async (req, res) => {
+  let decodedUser;
+
+  if (req.cookies.token != null) {
+    decodedUser = jwt.verify(req.cookies.token, process.env.JWTSECRET);
+  }
+
+  if (decodedUser == null) {
+    return res.render("discoverGroup", { isLoggedIn: false });
+  } else {
+    await db.collection("users").updateOne(
+      { username: decodedUser.username },
+      {
+        $push: {
+          groupIds: new ObjectId(req.params.id),
+        },
+      }
+    );
+
+    await db.collection("groups").updateOne(
+      { _id: new ObjectId(req.params.id) },
+      {
+        $push: {
+          userIds: new ObjectId(decodedUser._id),
+        },
+      }
+    );
+
+    res.redirect(`/user/groups/${req.params.id}`);
   }
 });
 
