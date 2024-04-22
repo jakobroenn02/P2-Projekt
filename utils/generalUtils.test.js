03cf0159e6884dcbff65cf5d57ff95c79b3ae39c
Sakarias <49249574+SakariasM@users.noreply.github.com>
2024-04-22 07:24:01 +0200

    All utils tested

--- utils/generalUtils.test.js
+++ utils/generalUtils.test.js
@@ -1,5 +1,9 @@
-const generalUtils = require('./generalUtil');
+
+/*
+const { getRandomElement } = require("./generalUtil");
 
 test('Get a random number that is no bigger than length of array', () => {
-    expect(generalUtils.getRandomElement([1, 2, 3, 4, 5])).toBeLessThanOrEqual(5);
+    expect(getRandomElement([1, 2, 3, 4, 5])).toBeLessThanOrEqual(5);
+    expect(getRandomElement)
 });
+*/
