c1df7aaa0b386bfdac7331537257d692447d9b68
Sakarias <49249574+SakariasM@users.noreply.github.com>
2024-04-22 14:30:33 +0200

    Updated some tests

--- utils/discoverUtils.test.js
+++ utils/discoverUtils.test.js
@@ -1,14 +1,22 @@
-
-/*
-const discoverUtils = require('./discoverUtils');
+const { shuffleArray } = require('./discoverUtils');
 
 // Test shuffleArray function
 const testArray = [1, 2, 3, 4, 5];
-test('Returns a shuffled version of input array, that contains the same elements and is the same size', () => {
-    expect(discoverUtils.shuffleArray(testArray).length).toBe([1, 2, 3, 4, 5].length);
-    expect(discoverUtils.shuffleArray(testArray)).toContain(1, 2, 3, 4, 5);
+
+describe('shuffleArray', () => {
+  it('returns an array of the same length', () => {
+    const shuffledArray = shuffleArray(testArray);
+    expect(shuffledArray.length).toBe(testArray.length);
+  });
+
+  it('returns an array containing the same elements', () => {
+    const shuffledArray = shuffleArray(testArray);
+    testArray.forEach((element) => {
+      expect(shuffledArray).toContain(element);
+    });
+  });
 });
 
-*/
+
 
 
--- utils/generalUtils.test.js
+++ utils/generalUtils.test.js
@@ -1,9 +1,8 @@
 
-/*
+
 const { getRandomElement } = require("./generalUtil");
 
 test('Get a random number that is no bigger than length of array', () => {
     expect(getRandomElement([1, 2, 3, 4, 5])).toBeLessThanOrEqual(5);
     expect(getRandomElement)
 });
-*/
