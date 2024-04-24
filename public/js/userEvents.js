02ab3c6aad46c58f4b8dee2e02b9f11e847c1099
Sakarias <49249574+SakariasM@users.noreply.github.com>
2024-04-24 15:34:50 +0200

    WIP Added change month functionality

--- public/js/userEvents.js
+++ public/js/userEvents.js
@@ -1,3 +1,50 @@
+// Calendar functionality
+
+// Import elements
+const monthTitle = document.querySelector(".calendarMonth-title");
+const scaleDownMonthBtn = document.querySelector("#scaleDownMonth");
+const scaleUpMonthBtn = document.querySelector("#scaleUpMonth");
+
+
+// Add eventlisteners
+scaleDownMonthBtn.addEventListener("click", () => {
+  workingDate.setMonth(workingDate.getMonth() - 1);
+  displayMonth = stringifyMonth(workingDate);
+  monthTitle.textContent = displayMonth;
+});
+
+scaleUpMonthBtn.addEventListener("click", () => {
+  workingDate.setMonth(workingDate.getMonth() + 1);
+  displayMonth = stringifyMonth(workingDate);
+  monthTitle.textContent = displayMonth;
+});
+
+
+// Get current date
+const todayDate = new Date();
+
+// Misc operations before display
+let displayMonth = stringifyMonth(todayDate);
+let workingDate = todayDate;
+
+
+// Update HTML content
+monthTitle.textContent = displayMonth;
+
+
+// Functions
+function stringifyMonth(date) { // Takes date object and returns month in string format
+  date = date.toLocaleString("default", { month: "long" })
+  return date.charAt(0).toUpperCase() + date.slice(1);
+}
+
+
+
+
+
+
+
+
 // JS for creating new event
 
 let createEventButton = document.querySelector(
